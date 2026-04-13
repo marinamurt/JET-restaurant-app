2d6ad22a1fc9e6aa59bef48b1eb863747b3958fa
Marina <m.murtaza@student.utwente.nl>
2026-04-13 18:51:51 +0200

    For now took the restaurants names for the EC4M7RF postcode and displayed the name of the first 10 restaurants that match it (slice(0,10)). Will move on with the display of the other restaurant data points next.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const link = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF"
 
 const getRestaurants = async () => {
-    console.log("getting restaurants...");
     let response = await fetch(link);
-    let restData = await response.json();
-    console.log(restData)
 
+    let data = await response.json();
+
+    data.restaurants.slice(0,10).forEach(restaurant => {
+        const name = `<li>${restaurant.name}</li>`;
+
+        document.querySelector('div').insertAdjacentHTML('beforeend', name);
+
+    });
 }
-
 getRestaurants();
+
