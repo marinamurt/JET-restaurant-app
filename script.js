b8d0b5cc93a60138e3a9a320b1e9231b5781aaf8
Marina <m.murtaza@student.utwente.nl>
2026-04-14 16:45:17 +0200

    --After analyzing the assignment decided to add a search bar for the UK postcodes for better UI of the webpage (replaced the poscode at the end with {postcode} and added postcode paramenter for the getRestaurants function. -added event listener for search: on enter or on click (with button) + styling and formatting
    --Added a deals attribute for restaurants that was found in the API for a better look after consulting food delivery apps.
    
    --Added some comments for better readability

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const link = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF"
+//below the base API endpoint url
+const baseLink = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
 
-const getRestaurants = async () => {
-    let response = await fetch(link);
+//function to fetch the: restsaurant objects and the relevant data endpoints from the API
+const getRestaurants = async (postcode) => {
+    const link = baseLink.replace("{postcode}", postcode);
+
+    response = await fetch(link);
 
     let data = await response.json();
+    // clear previous results for the search
+    document.querySelector('#restaurants-container').innerHTML = "";
 
+    // for each restaurant object we only get the first 10 results
     data.restaurants.slice(0,10).forEach(restaurant => {
         const cuisines = restaurant.cuisines.map(cuisine => cuisine.name).join(",  ");
-        const logo = restaurant.logoUrl;
+        const deals = restaurant.deals.map(deal => deal.description).join(" ");
+
+//make a restaurant card using data from a restaurant object.
         const details = `
             <div class = "details">
-            <img class="rest-logo" src=${logo} alt="">
+            <img class="rest-logo" src=${restaurant.logoUrl} alt="">
+            <h3 class = "rest-deals"> ${deals}</h3>
                 <h2 class="rest-name">${restaurant.name}</h2>
                 <h4 class = "rest-rating">Rating: ${getStars(restaurant.rating.starRating)} (${restaurant.rating.starRating})</h4>
                 <p class = "rest-cuisines"><b>Cuisines:</b> ${cuisines}</p>
@@ -21,7 +31,40 @@ const getRestaurants = async () => {
         document.querySelector('#restaurants-container').insertAdjacentHTML('beforeend', details);
     });
 }
+
+//fetch function called here
+getRestaurants();
+
+//function to get the rating in form of stars (with rounding)
 const getStars = (rating) => {
     return '⭐'.repeat(Math.round(rating));
 }
-getRestaurants();
+
+
+
+// Code for the serch bar -- event listener to do the search
+document.addEventListener("DOMContentLoaded", () => {
+
+    const input = document.getElementById("postcode-input");
+    const button = document.getElementById("search-button");
+    // for click
+    button.addEventListener("click", () => {
+        const postcode = input.value.trim(); //remove accidental spaces
+        if (postcode) {
+            getRestaurants(postcode);
+        }
+    });
+
+    //for Enter
+    input.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            const postcode = input.value.trim();
+            if (postcode) {
+                getRestaurants(postcode);
+            }
+        }
+    });
+
+
+
+});
